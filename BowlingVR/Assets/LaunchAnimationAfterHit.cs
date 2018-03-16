03aac24cc856ceb4a0ad1b8008fbb2febb7f13f7
Julien Malengret <julien.malengret@gmail.com>
2018-03-16 15:25:54 +0100

    Afternoon stuffs

---
+++ BowlingVR/Assets/LaunchAnimationAfterHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchAnimationAfterHit : MonoBehaviour {
+
+    public PlayMakerFSM stateMachine;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        stateMachine.SetState("Active");
+    }
+}
